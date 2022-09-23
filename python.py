1ee6b952c6238ce85a1d5215ac43485ddc88900e
sanraj2000 <70739919+sanraj2000@users.noreply.github.com>
2022-09-23 21:12:19 +0530

    Update python.py

--- python.py
+++ python.py
@@ -46,9 +46,14 @@ soup = BeautifulSoup(url_contents, "html")
 div = soup.find("div", {"id": "home-template"})
 div2 =soup.find("div",{"class": "homepage-new__hero__download"})
 div3= soup.find("div", {"class": "download-button__text-elements"})
+con = str(div)
 content = str(div2.text)
 content2 = str(div3.text)
 
+
+print(content[:50])
+
+print("only the div class")
 print(content)
 print("sandy sandy sandy")
 print(content2)
