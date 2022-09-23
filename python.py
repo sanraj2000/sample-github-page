2c8a2589fe7e81b0bedb5f0caf0d5eacda1d4236
sanraj2000 <70739919+sanraj2000@users.noreply.github.com>
2022-09-23 19:15:29 +0530

    Update python.py

--- python.py
+++ python.py
@@ -5,14 +5,33 @@
 #print(div_bs4.string)
 
 
-import urllib
-import requests
+#import urllib
+#import requests
 #from selenium import webdriver
 #driver = webdriver.Chrome(/path/ToChromeWebDriver)
-url = "https://fr.tradingview.com/markets/cryptocurrencies/global-charts/"
+#url = "https://fr.tradingview.com/markets/cryptocurrencies/global-charts/"
 #driver.get(url)
-html = urllib.request.urlopen(url).read()
+#html = urllib.request.urlopen(url).read()
 
-soup = BeautifulSoup(html, 'html.parser')
-for title in soup.find_all("div", {"class": "tabTitle-qQlkPW5Y"}):
-     print(title.string)
+#soup = BeautifulSoup(html, 'html.parser')
+#for title in soup.find_all("div", {"class": "tabTitle-qQlkPW5Y"}):
+ #    print(title.string)
+
+     
+  
+
+import requests
+from bs4 import BeautifulSoup
+
+link = "https://www.architecture.com/FindAnArchitect/FAAPractices.aspx?display=50"
+
+html = requests.get(link).text
+
+"""If you do not want to use requests then you can use the following code below 
+   with urllib (the snippet above). It should not cause any issue."""
+soup = BeautifulSoup(html, "lxml")
+res = soup.findAll("article", {"class": "listingItem"})
+for r in res:
+    print("Company Name: " + r.find('a').text)
+    print("Address: " + r.find("div", {'class': 'address'}).text)
+    print("Website: " + r.find_all("div", {'class': 'pageMeta-item'})[3].text)
