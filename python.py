9ef3ee64c93b24bd8c8ceb03b6f6d25280bb8cb4
sanraj2000 <70739919+sanraj2000@users.noreply.github.com>
2022-09-23 18:51:31 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,5 +1,15 @@
-from bs4 import BeautifulSoup
-markup = '''<html><body><div id="container">Div Content</div></body></html>'''
-soup = BeautifulSoup(markup, 'html.parser')
-div_bs4 = soup.find('div', id = "container")
-print(div_bs4.string)
+#from bs4 import BeautifulSoup
+#markup = '''<html><body><div id="container">Div Content</div></body></html>'''
+#soup = BeautifulSoup(markup, 'html.parser')
+#div_bs4 = soup.find('div', id = "container")
+#print(div_bs4.string)
+
+from selenium import webdriver
+driver = webdriver.Chrome(pathToChromeWebDriver)
+url = "https://fr.tradingview.com/markets/cryptocurrencies/global-charts/"
+driver.get(url)
+html = driver.page_source
+
+soup = BeautifulSoup(html, 'html.parser')
+for title in soup.find_all("div", {"class": "tabTitle-qQlkPW5Y"}):
+     print(title.string)
