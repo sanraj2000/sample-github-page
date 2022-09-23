25988b4cba322fc31301a97fd9ffe03eb454a387
sanraj2000 <70739919+sanraj2000@users.noreply.github.com>
2022-09-23 18:29:01 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-markup = '''<html><body><div id="container">Div Content</div></body></html>''
+markup = '''<html><body><div id="container">Div Content</div></body></html>'''
 soup = BeautifulSoup(markup, 'html.parser')
 div_bs4 = soup.find('div', id = "container")
 print(div_bs4.string)
