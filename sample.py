c1fdccfb44b8387379c5464e61d0258762b688fe
sanraj2000 <70739919+sanraj2000@users.noreply.github.com>
2022-09-25 12:08:24 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,21 +2,23 @@ import urllib
 import requests
 from bs4 import BeautifulSoup
 url = "https://vmapigwjenkins:8443/view/APIGW%20Cloud/job/YAIC-AZURE-SPRO-1011-SPLIT-RUN/"
-url_contents = urllib.request.urlopen(url).read()
+r = requests.get(url)
+print(r.content)
+#url_contents = urllib.request.urlopen(url).read()
 
-soup = BeautifulSoup(url_contents, "html")
+#soup = BeautifulSoup(url_contents, "html")
 #div = soup.find("div", {"id": "home-template"})
-div2 =soup.find("div",{"class": "build-icon"})
+#div2 =soup.find("div",{"class": "build-icon"})
 #div3= soup.find("div", {"class": "download-button__text-elements"})
 #con = str(div)
-content = str(div2.text)
+#content = str(div2.text)
 #content2 = str(div3.text)
 
 
 #print(con[:50])
 #print(con)
 
-print("only the div class")
-print(content)
+#print("only the div class")
+#print(content)
 #print("sandy sandy sandy")
 #print(content2)
