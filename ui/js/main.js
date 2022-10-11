fccd77d6571d34270ab7e5c3a5e2907e9571c24a
sanraj2000 <70739919+sanraj2000@users.noreply.github.com>
2022-10-11 10:25:41 +0530

    Update main.js

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,10 +1,10 @@
 function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = document.getElementsByClassName("tabcontentv");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablinks");
+  tablinks = document.getElementsByClassName("tv tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
@@ -13,4 +13,5 @@ function openCity(evt, cityName) {
 }
 
 // Get the element with id="defaultOpen" and click on it
+
 document.getElementById("defaultOpen").click();
